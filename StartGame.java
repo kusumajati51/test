059621eae2d4e6b2c258ddd7de0909a90ee9ff16
kusumajati51@gmail.com <kusuma12>
2019-09-14 15:58:37 +0700

    fix bug

--- StartGame.java
+++ StartGame.java
@@ -10,7 +10,7 @@ class StartGame {
   System.out.print("how many box you want to play: ");
     int angka  = put.nextInt();
     if(angka < 3){
-      System.out.print("Your number is lees than 3");
+      System.out.print("Your number is lees than 3 \n ");
       justPlay();
     }else{
       MyTictactoe tictactoe = new MyTictactoe(angka);
