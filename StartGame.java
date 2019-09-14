b156d9c96fee2c1b475381d794042d06ebb4dd5f
kusumajati51@gmail.com <kusuma12>
2019-09-14 12:30:56 +0700

    my test

--- StartGame.java
+++ StartGame.java
@@ -4,9 +4,13 @@ public class StartGame{
     private static Scanner put = new Scanner(System.in);
 
     public static void main(String[] args){
-        System.out.print("Silahkan masukan angka anda");
+        System.out.print("Silahkan masukan angka anda: ");
         int angka  = put.nextInt();
-        MyTictactoe tictactoe = new MyTictactoe(angka);
-        tictactoe.start();
+        if (angka < 3) {
+            System.out.print("Angka Anda kurang dari 3");
+        } else {
+            MyTictactoe tictactoe = new MyTictactoe(angka);
+            tictactoe.start();
+        }
     }
 }
